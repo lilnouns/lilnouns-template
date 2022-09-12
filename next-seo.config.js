bc8dc03535843608e8fce92a4aa0be929903458c
Milad Nekofar <milad@nekofar.com>
2022-09-12 12:18:16 +0430

    refactor: add default seo config and information

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://lilnouns.dev/',
+    site_name: 'Lil Nouns',
+  },
+  twitter: {
+    handle: '@nekofar',
+    site: '@nekofar',
+    cardType: 'summary_large_image',
+  }
+}
