00aa57fb91ae6343cb6c89cbced7565a0a4477ef
Milad Nekofar <milad@nekofar.com>
2023-11-06 19:52:12 +0330

    chore: update `webpack` configuration in `next.config.js`
    The webpack configuration in the `next.config.js` file has been refactored. The fallback configurations were expanded for better resolution and "fs" module handling was disabled as it is not compatible with browsers. This change ensures smoother webpack bundling and prevents runtime errors related to modules intended for native node environments.

--- next.config.js
+++ next.config.js
@@ -1,44 +1,65 @@
-const incstr = require('incstr');
+const incstr = require('incstr')
 
-const classNames = {};
+const classNames = {}
 
 const generateClassName = incstr.idGenerator({
-  alphabet: 'abcdefghijklmnopqrstuvwxyz'
-});
+  alphabet: 'abcdefghijklmnopqrstuvwxyz',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, {dev, isServer}) => {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    webpackBuildWorker: true,
+    swcPlugins: [
+      [
+        '@lingui/swc-plugin',
+        {
+          // the same options as in .swcrc
+        },
+      ],
+    ],
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.resolve.fallback = {
+        // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped.
+        ...config.resolve.fallback,
 
-    if (!dev) {
-      const MangleCssClassPlugin = require('mangle-css-class-webpack-plugin');
-      config.plugins.push(new MangleCssClassPlugin({
-        classNameRegExp: '(([a-zA-Z-:]*)[\\\\\\\\]*:)*([\\\\\\\\]*!)?-?tw-[a-zA-Z-]([a-zA-Z0-9-]*([\\\\\\\\]*(\\%|\\#|\\.|\\[|\\]|\\/))*)*',
-        log: process.env.NODE_ENV === 'development',
-        classGenerator: (original, opts, context) => {
-          if (classNames[original]) {
-            return classNames[original];
-          }
-
-          let nextId;
-
-          do {
-            // Class name cannot start with a number.
-            nextId = generateClassName();
-          } while (/^[0-9_-]/.test(nextId));
-
-          return (classNames[original] = nextId);
-        }
-      }));
+        fs: false,
     }
 
-    return config;
+    if (!dev) {
+      const MangleCssClassPlugin = require('mangle-css-class-webpack-plugin')
+      config.plugins.push(
+        new MangleCssClassPlugin({
+          classNameRegExp:
+            '(([a-zA-Z-:]*)[\\\\\\\\]*:)*([\\\\\\\\]*!)?-?tw-[a-zA-Z-]([a-zA-Z0-9-]*([\\\\\\\\]*(\\%|\\#|\\.|\\[|\\]|\\/))*)*',
+          log: process.env.NODE_ENV === 'development',
+          classGenerator: (original, opts, context) => {
+            if (classNames[original]) {
+              return classNames[original]
+            }
+
+            let nextId
+
+            do {
+              // Class name cannot start with a number.
+              nextId = generateClassName()
+            } while (/^[0-9_-]/.test(nextId))
+
+            return (classNames[original] = nextId)
+          },
+        }),
+      )
+    }
+
+    return config
   },
 }
 
