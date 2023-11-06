8646f0b664b813b71683fe5df8da76fe9887c4ef
Milad Nekofar <milad@nekofar.com>
2023-11-06 20:31:21 +0330

    refactor: add tests and localization functionality for Home page.
    Added unit tests under 'index.test.tsx' for Home component to verify locale-specific redirection and proper retrieval of static props based on locale. In 'index.ts', incorporated usage of localization utilities for page routing and component rendering. These changes improve maintainability and localization support of the application.

---
+++ src/pages/index.ts
@@ -0,0 +1,19 @@
+import { getLocale, loadCatalog } from '@/utils'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace(`/${getLocale()}`)
+  }, [router])
+
+  return null
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: { translation: await loadCatalog(getLocale()) },
+  }
+}
